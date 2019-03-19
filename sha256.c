5f1c59ff8ee4ea44c1decdf41d8b7d0d82f60534
Tianle97 <386466921@qq.com>
2019-03-19 13:10:19 +0000

    Finish Starting to code the SHA-256 algorithm.

--- sha256.c
+++ sha256.c
@@ -21,6 +21,10 @@ void sha256();
 uint32_t sig0(uint32_t x);
 uint32_t sig1(uint32_t x);
 
+//See Sections 3.2 for definitions
+uint32_t rotr(uint 32_t n, uint32_t x);
+uint32_t rotr(uint 32_t n, uint32_t x)v;
+
 int main(int argc, char *argv[]) {
 
 		sha256();
@@ -65,15 +69,57 @@ void sha256(){
 		//From page 22 W[t]=...
 		for(i = 16; i < 64; i++)
 				sig_1(W[t-2]) + W[t-7] + sig_0(W[t-15]) + W[t-16];
+
+		//Initialise a, b, c, ... , h as per Step 2, Page 22
+		a = H[0]; b = H[1]; c = H[2]; d = H[3];
+	 	e = H[4]; f = H[5]; g = H[6]; h = H[7];
+
+		//Steps 3.
+		for (t = 0; t < 64; t++) {
+				T1 = h + SIG_1(e) + Ch(e, f, g) + K[t] + W[t];
+				T2 = SIG_0(a) + Maj(a, b, c);
+				h = g;
+				g = f;
+				f = e;
+				e = d + T1;
+				d = c;
+				c = b;
+				b = a;
+				a = T1 _ T2;
+		}
+
+		//Step 4.
+		H[0] = a + H[0];
+		H[1] = b + H[1];
+		H[2] = c + H[2];
+		H[3] = d + H[3];
+		H[4] = e + H[4];
+		H[5] = f + H[5];
+		H[6] = g + H[6];
+		H[7] = h + H[7];
+
 		
 }
 
-uint32_t sig0(uint32_t x) {
+//See Sections 3.2 for definitions
+uint32_t rotr(uint 32_t n, uint32_t x) {
+	return (x >> n) | ( x<< (32 - n));
+}
 
+uint32_t rotr(uint 32_t n, uint32_t x)v {
+	return (x >> n);
+}
+
+uint32_t sig0(uint32_t x) {
+	// See Sections 3.2 and 4.1.2 for definitions
+	// ROTR_x(x) = (x >> n) | <x << (32 - n)
+	// SHR_n(x) = (x >> n)
+	return (rotr(7,x) ^ rotr(18,x) ^ shr(3,x));
 }
 
 uint32_t sig1(uint32_t x) {
-
+  // See Sections 3.2 and 4.1.2 for definitions
+	return (rotr(17,x) ^ rotr(19,x) ^ shr(10,x));
 }
 
 
