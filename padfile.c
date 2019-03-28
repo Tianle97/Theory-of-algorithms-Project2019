beebd655b35ac724c24bd693208d93c95ad429ed
Tianle97 <386466921@qq.com>
2019-03-28 23:54:37 +0000

    fix some problem and add some comments

--- padfile.c
+++ padfile.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 #include <stdint.h>
 
-struct point {
-    uint8_t x[64];
-    uint32_t y[16];
+union msgblock {
+    uint8_t e[64];
+    uint32_t t[16];
     uint64_t s[8];
 };
 
 int main(int argc,char *argv[]) {
     
-    union msgblock M;
-    
+    union msgblock M;  
     uint64_t nobytes;
     
     FILE* f;
-    
-    char c;
-    
     //Open the file
+    
     f = fopen(argv[1],"r");
     
     while (!feof(f)) {
-        fread(M.e,1, 64, f);
-        printf("%llu",nobytes);
+        // get file size
+        nobytes = fread(M.e,1, 64, f);
+        if(nobytes < 56) {
+            printf("I've found a with less than 55 bytes!\n");
+            M.e[nobytes] = 0x01;
+        } 
+        
+        
+        printf("%llu\n",nobytes);
+        
     }
     
-    print("%c\n", fread(&c, 1, 1, f));
-    
-    //Close the file
     fclose(f);
     
     return 0;
