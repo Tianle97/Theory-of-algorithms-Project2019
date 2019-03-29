19107586f5fa8f295884c9e7170c5839a92d791b
Tianle97 <386466921@qq.com>
2019-03-29 00:37:55 +0000

    add some codes of the SHA256 padding.

--- padfile.c
+++ padfile.c
@@ -10,27 +10,37 @@ union msgblock {
 int main(int argc,char *argv[]) {
     
     union msgblock M;  
+    
+    uint64_t nobits = 0;
+    
     uint64_t nobytes;
     
     FILE* f;
     //Open the file
-    
     f = fopen(argv[1],"r");
     
+    //feof()是函数，用来检测文件的结束；
     while (!feof(f)) {
         // get file size
-        nobytes = fread(M.e,1, 64, f);
+        nobytes = fread(M.e, 1, 64, f);
+        printf("Ready %2llu \n bytes",nobytes);
+        nobits = nobits + (nobytes * 8);
         if(nobytes < 56) {
             printf("I've found a with less than 55 bytes!\n");
-            M.e[nobytes] = 0x01;
+            M.e[nobytes] = 0x80;
+            while(nobytes < 56) {
+                nobytes = nobytes + 1;
+                M.e[nobytes] = 0x00;
+            }
+            M.s[7] = nobits;
         } 
-        
-        
-        printf("%llu\n",nobytes);
-        
     }
     
     fclose(f);
+    
+    for(int i = 0; i < 64; i++)
+        printf("%x ",M.e[i]);
+    printf("\n");
     
     return 0;
 }
